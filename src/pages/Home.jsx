4b1e45962e2354009c262d6291b56d3a20fff4ef
Tiago Ferreira <88331599+Killuasito@users.noreply.github.com>
2026-03-31 14:10:02 -0300

    Update description for Sarau FICS to version 2.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
         </h1>
 
         <p className="text-xl text-gray-700 mb-8">
-          Gere seu certificado do Sarau FICS de forma rápida e simples.
+          Gere seu certificado do Sarau FICS 2.0 de forma rápida e simples.
         </p>
 
         <Link
